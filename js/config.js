ed02f5a73b060abaa8be99fd1e8c622940b882ef
VarunBarad <barad.varun29@gmail.com>
2016-03-28 11:25:39 +0530

    Update different status of SMS.
    Update the different status of SMS to reflect actual usage.

--- js/config.js
+++ js/config.js
@@ -5,8 +5,9 @@
 var config = {
     baseUrl: 'http://ec2-52-32-39-143.us-west-2.compute.amazonaws.com:8001',
     status: {
-        delivered: 'D',
-        error: 'E',
+        delivered: ['D'],
+        error: ['U', 'E', 'I'],
+        pending: ['P'],
         notApplicable: 'NA'
     }
 };
